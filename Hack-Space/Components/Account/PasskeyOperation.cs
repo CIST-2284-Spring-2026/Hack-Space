f62a3e246c777f8d311adb413de2c30b493e8105
Joshua Madrid <jmadrid116@cnm.edu>
2026-02-05 21:13:30 -0700

    new blazor

---
+++ Hack-Space/Components/Account/PasskeyOperation.cs
@@ -0,0 +1,7 @@
+namespace Hack_Space.Components.Account;
+
+public enum PasskeyOperation
+{
+    Create = 0,
+    Request = 1,
+}
---
+++ Hack-Space/Data/ApplicationDbContext.cs
@@ -0,0 +1,8 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Hack_Space.Data;
+
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
+{
+}
