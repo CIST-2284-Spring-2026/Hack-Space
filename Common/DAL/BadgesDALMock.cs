8b3dacdb392506fd5938b4cffa8c3aeb66722e1a
Joshua Madrid <jmadrid116@cnm.edu>
2026-02-19 19:12:56 -0700

    Badge Edit Page

--- Common/DAL/BadgesDALMock.cs
+++ Common/DAL/BadgesDALMock.cs
@@ -1,24 +1,47 @@
 using Common.Interaces;
 using Common.Models;
-
-namespace Common.DAL;
  
-public class BadgesDALMock : IBadgesDAL
+namespace Common.DAL
 {
-    // Local in-memory data for development/testing
-    private List<Badge>? badges = new List<Badge>() 
+    public class BadgesDALMock : IBadgesDAL
     {
-        new Badge { Title = "Contributor", Description = "Awarded for contributing to the project.", 
-        TurnInInstructions = "Submit a pull request with your contribution." }, 
-        new Badge { Title = "Early Adopter", Description = "Awarded for being an early adopter of the platform.", 
-        TurnInInstructions = "Sign up and start using the platform early." },
-        new Badge { Title = "Community Helper", Description = "Awarded for helping other members of the community.", 
-        TurnInInstructions = "Assist other members in the community forums." }
-    };
+        private List<Badge>? badges = new List<Badge>()
+        {
+            new Badge {Id = Guid.NewGuid(), Title = "Contributor", Description = "Awarded for contributing to the project.", TurnInInstructions = "Submit a pull request with your contribution." },
+            new Badge {Id = Guid.NewGuid(), Title = "Early Adopter", Description = "Awarded for being an early adopter of the platform.", TurnInInstructions = "Sign up and start using the platform early." },
+            new Badge {Id = Guid.NewGuid(), Title = "Community Helper", Description = "Awarded for helping other members of the community.", TurnInInstructions = "Assist other members in the community forums." }
+        };
  
-    public Task<List<Badge>?> GetBadgesAsync()
-    {
-        return Task.FromResult(badges);
+        public Task AddBadgeAsync(Badge badge)
+        {
+            badge.Id = Guid.NewGuid();
+            badges?.Add(badge);
+            return Task.CompletedTask;
+        }
+ 
+        public Task<Badge?> GetBadgeByIdAsync(Guid id)
+        {
+            var badge = badges?.Where(b => b.Id == id).FirstOrDefault();
+            return Task.FromResult(badge);
+        }
+ 
+        public Task<List<Badge>?> GetBadgesAsync()
+        {
+            return Task.FromResult(badges);
+        }
+ 
+        public Task UpdateBadgeAsync(Badge badge)
+        {
+            // find the badge in the list and update it
+            var existingBadge = badges?.Where(b => b.Id == badge.Id).FirstOrDefault();
+            if (existingBadge != null)
+            {
+                existingBadge.Title = badge.Title;
+                existingBadge.Description = badge.Description;
+                existingBadge.TurnInInstructions = badge.TurnInInstructions;
+                existingBadge.IsVisible = badge.IsVisible;
+            }
+            return Task.CompletedTask;
+        }
     }
-
 }
--- Common/Interfaces/IBadgesDAL.cs
+++ Common/Interfaces/IBadgesDAL.cs
@@ -5,5 +5,8 @@ namespace Common.Interaces
     public interface IBadgesDAL
     {
         public Task<List<Badge>?> GetBadgesAsync();
+        public Task AddBadgeAsync(Badge badge);
+        public Task<Badge?> GetBadgeByIdAsync(Guid id);
+        public Task UpdateBadgeAsync(Badge badge);
     }
 }
