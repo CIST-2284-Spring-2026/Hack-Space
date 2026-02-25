95448f3646d4b994d04d8e9f4ee6539bf62e957b
Joshua Madrid <jmadrid116@cnm.edu>
2026-02-24 18:57:33 -0700

    Detailed View

--- Common/Interfaces/IBadgesDAL.cs
+++ Common/Interfaces/IBadgesDAL.cs
@@ -5,8 +5,8 @@ namespace Common.Interaces
     public interface IBadgesDAL
     {
         public Task<List<Badge>?> GetBadgesAsync();
-        public Task AddBadgeAsync(Badge badge);
         public Task<Badge?> GetBadgeByIdAsync(Guid id);
+        public Task AddBadgeAsync(Badge badge);
         public Task UpdateBadgeAsync(Badge badge);
     }
 }
